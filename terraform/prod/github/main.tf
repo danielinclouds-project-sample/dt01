6acc05bf3b667f81ce67555d75bccc6f257d73e6
Daniel Kozlowski <dkozlowski.external@epo.org>
2024-04-09 00:27:27 +0100

    play

--- terraform/prod/github/main.tf
+++ terraform/prod/github/main.tf
@@ -10,17 +10,19 @@ terraform {
 provider "github" {}
 
 
-data "github_repository" "repo" {
-  full_name = "danielinclouds-project-sample/dt01"
-}
+# data "github_repository" "repo" {
+#   full_name = "danielinclouds-project-sample/dt01"
+# }
 
 resource "github_repository_environment" "repo_environment" {
-  repository       = data.github_repository.repo.name
+#   repository       = data.github_repository.repo.name
+  repository       = "danielinclouds-project-sample/dt01"
   environment      = "production"
 }
 
 resource "github_actions_variable" "example_variable" {
-  repository       = data.github_repository.repo.name
+#   repository       = data.github_repository.repo.name
+  repository       = "danielinclouds-project-sample/dt01"
   variable_name    = "example_variable_name"
   value            = "example_variable_value"
 }
