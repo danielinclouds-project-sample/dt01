d37e733b80a7ceb8026ae76fa2579fd130b30428
Daniel Kozlowski <dkozlowski.external@epo.org>
2024-04-09 00:21:56 +0100

    pipeline and github tf code

---
+++ terraform/prod/github/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+    }
+  }
+}
+
+provider "github" {}
+
+
+data "github_repository" "repo" {
+  full_name = "danielinclouds-project-sample/dt01"
+}
+
+resource "github_repository_environment" "repo_environment" {
+  repository       = data.github_repository.repo.name
+  environment      = "production"
+}
+
+resource "github_actions_variable" "example_variable" {
+  repository       = data.github_repository.repo.name
+  variable_name    = "example_variable_name"
+  value            = "example_variable_value"
+}
+
